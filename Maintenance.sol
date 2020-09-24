5596cfb2b5812eefeb618d43ad47336441789fb7
Francesca Cella <francescacella96@gmail.com>
2020-09-24 08:06:46 +0200

    Update Maintenance.sol

--- Maintenance.sol
+++ Maintenance.sol
@@ -1,31 +1,55 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
 
 import '../../OpenZeppelin/openzeppelin-contracts/contracts/math/SafeMath.sol';
 
 contract Maintenance {
     using SafeMath for uint256;
-
+    
     struct Data {
-        string name;
-        address registerdBy; //operator address
-        //TODO: which information needs to be saved?
+        string description;
+        uint256 portion;
+        address registerdBy;
     }
     
     struct Operator {
-        uint256[] maintenanceRegistered;
+        uint256[] maintenancesRegistered;
+    }
+    
+    struct Portion {
+        uint256[] maintenancesPerformed;
     }
     
     mapping (uint256 => Data) private maintenances;
     mapping (address => Operator) private operators;
+    mapping (uint256 => Portion) private portions;
     
     uint lastMaintenanceId;
-    
-    modifier onlyOperator(uint256 _maintenanceId) {
-        require(maintenances[_maintenanceId].registerdBy == msg.sender);
-        _;
-    }
 
-    //TODO: getters and setters
+    function register(string calldata _description, uint256 _id) external {
+        maintenances[lastMaintenanceId].description = _description;
+        maintenances[lastMaintenanceId].portion = _id;
+        maintenances[lastMaintenanceId].registerdBy = msg.sender;
+        
+        
+        operators[msg.sender].maintenancesRegistered.push(lastMaintenanceId);
+        
+        portions[_id].maintenancesPerformed.push(lastMaintenanceId);
+        
+        lastMaintenanceId++;
+    }
+    
+    function getById(uint256 _id) external view returns (Data memory) {
+        return maintenances[_id];
+    }
+    
+    function getByOperator(address _address) external view returns (Operator memory) {
+        return operators[_address];
+    }
+    
+    function getByPortion(uint256 _id) external view returns (Portion memory) {
+        return portions[_id];
+    }
 }
