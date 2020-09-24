fad60092cdb8d7a551d7191ca5d2d9aa9c80b25c
Francesca Cella <francescacella96@gmail.com>
2020-09-24 08:36:20 +0200

    Update Portion.sol

--- Portion.sol
+++ Portion.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
 
 import '../../OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol';
 import '../../OpenZeppelin/openzeppelin-contracts/contracts/math/SafeMath.sol';
@@ -9,6 +10,10 @@ import './Storage.sol';
 
 contract Portion is ERC721 {
     using SafeMath for uint256;
+    
+    struct Data {
+        string description;
+    }
     
     struct Owner {
         uint256[] portionsOwned;
@@ -19,20 +24,21 @@ contract Portion is ERC721 {
     }
     
     struct TermsOfSale {
-        uint256 price; //TODO: float
+        uint256 price;
         uint256 duration; //perpetual or for n years
         string expectedProduction;
-        string periodicity; //TODO: improve this field
-        uint256 expectedMaintenanceCost; //TODO: float
-        uint256 expectedProdActivityCost; //TODO: float
+        string periodicity;
+        uint256 expectedMaintenanceCost;
+        uint256 expectedProdActivityCost;
         uint256 landId;
         address owner;
         address buyer; //this will be filled after the trading
     }
     
+    mapping (uint256 => Data) private portions;
     mapping (address => Owner) private owners;
     mapping (address => Buyer) private buyers;
-    mapping (uint256 => TermsOfSale) private portions;
+    mapping (uint256 => TermsOfSale) private portionTerms;
     
     uint256 private lastPortionId;
     
@@ -43,23 +49,23 @@ contract Portion is ERC721 {
     }
     
     modifier onlyOwner(uint256 _portionId) {
-        require(portions[_portionId].owner == msg.sender);
+        require(portionTerms[_portionId].owner == msg.sender);
         _;
     }
     
     modifier onlyOwnerAndBuyer(uint256 _portionId) {
-        require(portions[_portionId].owner == msg.sender || portions[_portionId].buyer == msg.sender);
+        require(portionTerms[_portionId].owner == msg.sender || portionTerms[_portionId].buyer == msg.sender);
         _;
     }
     
-    function register(uint256 _landId, bytes32 _data) external {
+    function register(uint256 _landId, string calldata _description, bytes32 _data) external {
+        portions[lastPortionId].description = _description;
         
-        //TODO: test these lines
         TermsOfSale memory terms;
         terms.landId = _landId;
         terms.owner = msg.sender;
         
-        portions[lastPortionId + 1] = terms;
+        portionTerms[lastPortionId] = terms;
         dataStorage.add(_data);
         lastPortionId++;
     }
@@ -81,21 +87,23 @@ contract Portion is ERC721 {
         terms.expectedMaintenanceCost = _expectedMaintenanceCost;
         terms.expectedProdActivityCost = _expectedProdActivityCost;
 
-        portions[_portionId] = terms;
+        portionTerms[_portionId] = terms;
     }
     
     function sell(uint256 _portionId, address _buyer) external onlyOwnerAndBuyer(_portionId) { //sell and transfer ownership
-        portions[_portionId].buyer = _buyer; //TODO: improve
+        portionTerms[_portionId].buyer = _buyer;
     }
     
-    //TODO: which information need to be saved?
-    function browseHistory(uint256 _portionId) external {
-        
+    function get(uint256 _id) external view returns (Data memory, TermsOfSale memory) {
+        return (portions[_id], portionTerms[_id]);
     }
     
-    //TODO: ownership expiration?
-    function ownershipExpiration(uint256 _portionId) external {
-        
+    function getByOwner(address _owner) external view returns (Owner memory) {
+        return owners[_owner];
+    }
+    
+    function getByBuyer(address _buyer) external view returns (Buyer memory) {
+        return buyers[_buyer];
     }
     
 }
