8397c4ed3616b77e60aad209c6ae46ed2d6d5533
Francesca Cella <francescacella96@gmail.com>
2020-09-27 15:24:41 +0200

    Update Storage.sol

--- Storage.sol
+++ Storage.sol
@@ -7,23 +7,18 @@ import '../../OpenZeppelin/openzeppelin-contracts/contracts/math/SafeMath.sol';
 
 contract Storage {
     using SafeMath for uint256;
-
-    struct Item {
-        bytes32 itemHash;
-    }
     
-    
-    mapping (uint256 => Item) private items;
+    mapping (uint256 => bytes32) private items;
     uint private lastItemId;
     
     function add(bytes32[] memory _item) public returns(uint256){
-        items[lastItemId] = Item({ itemHash: keccak256(abi.encodePacked(_item)) });
+        items[lastItemId] = keccak256(abi.encodePacked(_item));
         lastItemId++;
         
         return lastItemId - 1;
     }
     
-    function get(uint256 id) public view returns(Item memory) {
+    function getbYiD(uint256 id) public view returns(bytes32) {
         return items[id];
     }
 }
