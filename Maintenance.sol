36f8f5b6963cf5dd015f58c7c92ad5a6ba0b862a
Francesca Cella <francescacella96@gmail.com>
2020-09-27 14:40:57 +0200

    Update Maintenance.sol

--- Maintenance.sol
+++ Maintenance.sol
@@ -4,56 +4,6 @@ pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
 import '../../OpenZeppelin/openzeppelin-contracts/contracts/math/SafeMath.sol';
+import './Recordable.sol';
 
-contract Maintenance {
-    using SafeMath for uint256;
-    
-    struct Data {
-        string description;
-        uint256 portion;
-        address registerdBy;
-    }
-    
-    struct Operator {
-        uint256[] maintenancesRegistered;
-    }
-    
-    struct Portion {
-        uint256[] maintenancesPerformed;
-    }
-    
-    mapping (uint256 => Data) private maintenances;
-    mapping (address => Operator) private operators;
-    mapping (uint256 => Portion) private portions;
-    
-    uint lastMaintenanceId;
-
-    function register(string calldata _description, uint256 _id) external {
-        maintenances[lastMaintenanceId].description = _description;
-        maintenances[lastMaintenanceId].portion = _id;
-        maintenances[lastMaintenanceId].registerdBy = msg.sender;
-        
-        
-        operators[msg.sender].maintenancesRegistered.push(lastMaintenanceId);
-        
-        portions[_id].maintenancesPerformed.push(lastMaintenanceId);
-        
-        lastMaintenanceId++;
-    }
-    
-    function getById(uint256 _id) external view returns (Data memory) {
-        return maintenances[_id];
-    }
-    
-    function getByOperator(address _address) external view returns (Operator memory) {
-        return operators[_address];
-    }
-    
-    function getByPortion(uint256 _id) external view returns (Portion memory) {
-        return portions[_id];
-    }
-    
-    function getTotalMaintenances() external view returns (uint256) {
-        return lastMaintenanceId;
-    }
-}
+contract Maintenance is Recordable {}
