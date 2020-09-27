bdba7615d494fd87d955fb815b3183b373c64bc6
Francesca Cella <francescacella96@gmail.com>
2020-09-27 17:43:30 +0200

    Update Certifiable.sol

--- Certifiable.sol
+++ Certifiable.sol
@@ -11,28 +11,21 @@ contract Certifiable {
     struct Certification {
         string description;
         address certifier;
-        uint256 item;
     }
     
     uint256 private lastCertificationId;
     
-    mapping (uint256 => uint256[]) private certificationsByItem;
     mapping (uint256 => Certification) private certifications;
     mapping (address => uint256[]) private certificationsByCertifiers;
     
     function certify(uint256 _id, string calldata _description) external {
         certifications[lastCertificationId].description = _description;
         certifications[lastCertificationId].certifier = msg.sender;
-        certifications[lastCertificationId].item = _id;
         
         certificationsByCertifiers[msg.sender].push(_id);
         
-        certificationsByItem[_id].push(lastCertificationId);
-        
         lastCertificationId ++;
     }
-    
-    
     
     function getCertificationById(uint256 _id) external view returns (Certification memory) {
         return certifications[_id];
@@ -40,9 +33,5 @@ contract Certifiable {
     
     function getByCertifier(address _address) external view returns (uint256[] memory) {
         return certificationsByCertifiers[_address];
-    }
-    
-    function getByItem(uint256 _id) external view returns (uint256[] memory) {
-        return certificationsByItem[_id];
     }
 }
