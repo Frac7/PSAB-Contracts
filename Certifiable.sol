3cb04a3f6499323ef7f653754b8455f2cb1a95a8
Francesca Cella <francescacella96@gmail.com>
2020-09-30 19:19:58 +0200

    Update Certifiable.sol

--- Certifiable.sol
+++ Certifiable.sol
@@ -6,19 +6,28 @@ pragma experimental ABIEncoderV2;
 import '../../OpenZeppelin/openzeppelin-contracts/contracts/math/SafeMath.sol';
 
 contract Certifiable {
+    /// @title Certifiable item
+    
     using SafeMath for uint256;
     
+    /// @dev A certification is represented by a description and the certifier's address
     struct Certification {
         string description;
         address certifier;
     }
     
+    /// @dev Certifications counter
     uint256 private lastCertificationId;
     
+    /// @dev All certifications registered
     mapping (uint256 => Certification) private certifications;
+    /// @dev Certification IDs grouped by certifier's address
     mapping (address => uint256[]) private certificationsByCertifiers;
+    /// @dev Certification IDs grouped by the related item
     mapping (uint256 => uint256[]) private certificationsByItems;
     
+    /// @param _id Item to be certified
+    /// @param _description Certification description
     function certify(uint256 _id, string calldata _description) external {
         certifications[lastCertificationId].description = _description;
         certifications[lastCertificationId].certifier = msg.sender;
@@ -30,14 +39,20 @@ contract Certifiable {
         lastCertificationId ++;
     }
     
+    /// @param _id Item for which to retrieve certifications
+    /// @return the related certification
     function getCertificationById(uint256 _id) external view returns (Certification memory) {
         return certifications[_id];
     }
     
+    /// @param _address Certifier address for which to retrieve certifications
+    /// @return the array containing all the certifications perfomed by a specific certifier
     function getByCertifier(address _address) external view returns (uint256[] memory) {
         return certificationsByCertifiers[_address];
     }
     
+    /// @param _item Item ID for which to retrieve certifications
+    /// @return the array containing all the certifications related to a specific item
     function getByItem(uint256 _item) external view returns (uint256[] memory) {
         return certificationsByItems[_item];
     }
