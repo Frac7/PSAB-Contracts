96857c83a2480dbf0ce07cd6e23b7251fdab3bf0
Francesca Cella <francescacella96@gmail.com>
2020-09-30 08:09:23 +0200

    Update Maintenance.sol

--- Maintenance.sol
+++ Maintenance.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
-pragma experimental ABIEncoderV2;
 
-import '../../OpenZeppelin/openzeppelin-contracts/contracts/math/SafeMath.sol';
 import './Recordable.sol';
 
 contract Maintenance is Recordable {}
