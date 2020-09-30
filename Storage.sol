973a2f3fe08b493e3e5b3356825b4ded3e02717c
Francesca Cella <francescacella96@gmail.com>
2020-09-30 19:47:25 +0200

    Update Storage.sol

--- Storage.sol
+++ Storage.sol
@@ -6,11 +6,17 @@ pragma experimental ABIEncoderV2;
 import '../../OpenZeppelin/openzeppelin-contracts/contracts/math/SafeMath.sol';
 
 contract Storage {
+    /// @title Contract for storing hased base64 documents
+    
     using SafeMath for uint256;
     
+    /// @dev Hashes by ID
     mapping (uint256 => bytes32) private items;
+    /// @dev Hashes counter
     uint private lastItemId;
     
+    /// @param _item Base64 document
+    /// @return the ID of the hash
     function add(string memory _item) public returns(uint256) {
         items[lastItemId] = keccak256(abi.encodePacked(_item));
         lastItemId++;
@@ -18,7 +24,9 @@ contract Storage {
         return lastItemId - 1;
     }
     
-    function getById(uint256 id) public view returns(bytes32) {
-        return items[id];
+    /// @param _id Hash ID
+    /// @return the hash related to the ID
+    function getById(uint256 _id) public view returns(bytes32) {
+        return items[_id];
     }
 }
