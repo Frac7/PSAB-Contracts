1025d1eb6d354cfa93148811469f202575a11a33
Francesca Cella <francescacella96@gmail.com>
2020-09-30 09:27:43 +0200

    Update Product.sol

--- Product.sol
+++ Product.sol
@@ -1,8 +1,15 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
 
+import '../../OpenZeppelin/openzeppelin-contracts/contracts/math/SafeMath.sol';
 import './Recordable.sol';
 import './Certifiable.sol';
 
-contract Product is Recordable, Certifiable {}
+contract Product is Recordable, Certifiable {
+    function certifyProduct(uint256 _id, string calldata _description) external {
+        if (this.getById(_id).registeredBy == address(0)) revert('Element does not exist');
+        this.certify(_id, _description);
+    }
+}
